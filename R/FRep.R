9d2ec01acd42d3f1a216009afd05e6513e31fd63
Jack-Wong94 <lwon229@aucklanduni.ac.nz>
2022-08-28 22:29:27 +1200

    Merge the repo

--- R/FRep.R
+++ R/FRep.R
@@ -4,6 +4,280 @@
 ################################################################################
 ## FRep library
 
+## hfSuperell(x,center,a,b,c,s1,s2)
+
+hfSuperell <- function(cx = 0, cy = 0, cz = 0, a = 1, b = 1, c = 1, s1 = 1, s2 = 1, name = NULL)
+{
+  if (!(is.numeric(cx) &&
+        is.numeric(cy) &&
+        is.numeric(cz) &&
+        is.numeric(a)  &&
+        is.numeric(b)  &&
+        is.numeric(c)  &&
+        is.numeric(s1)  &&
+        is.numeric(s2)))
+    stop("All arguments must be numeric")
+  
+  params <- cbind(cx, cy, cz, a, b, c, s1, s2)
+  colnames(params) <- c("x", "y", "z", "a", "b", "c", "s1", "s2")
+  name <- hfName(name, nrow(params))
+  superellipsoid <- list(params = params, name = name)
+  class(superellipsoid) <- c("HyperFunSuperellipsoid", "HyperFunObject", "HyperFun")
+  superellipsoid
+}
+
+as.character.HyperFunSuperellipsoid <- function(x, ...)
+{
+  center <- paste0("[", x$params[,"x"], ",", x$params[,"y"], "," , x$params[,"z"], "]")
+  paste0(x$name, "(x[3], a[1])", "\n",
+         "{", "\n",
+         "array center[3];", "\n",
+         "center = ", center, ";", "\n",
+         "superell = hfSuperell", "(x, center, ", x$params[,"a"], ",", x$params[,"b"], ",", x$params[,"c"], ",", x$params[,"s1"], ",", x$params[,"s2"] ,");", "\n",
+         x$name, " = superell;", "\n",
+         "}"
+  )
+}
+
+length.HyperFunSuperellipsoid <- function(x, ...){
+  nrow(x$param)  
+}
+
+## hfTorus
+
+hfTorus <- function(cx = 0, cy = 0, cz = 0, R = 1, r0 = 1, axis = "X", name = NULL)
+{
+  if (!(is.numeric(cx) &&
+        is.numeric(cy) &&
+        is.numeric(cz) &&
+        is.numeric(R)  &&
+        is.numeric(r0)))
+    stop("All arguments must be numeric")
+  axis = toupper(axis)
+  if (!all(axis %in% c("X", "Y", "Z")))
+  {
+    stop("Axis name must be 'X' or 'Y' or 'Z'")
+  }
+  params <- cbind(cx, cy, cz, R, r0, axis)
+  colnames(params) <- c("x", "y", "z", "R", "r0", "axis")
+  name <- hfName(name, nrow(params))
+  torus <- list(params = params, name = name)
+  class(torus) <- c("HyperFunTorus", "HyperFunObject", "HyperFun")
+  torus
+}
+
+as.character.HyperFunTorus <- function(x, ...)
+{
+  center <- paste0("[", x$params[,"x"], ",", x$params[,"y"], "," , x$params[,"z"], "]")
+  paste0(x$name, "(x[3], a[1])", "\n",
+         "{", "\n",
+         "array center[3];", "\n",
+         "center = ", center, ";", "\n",
+         "torus = hfTorus", x$params[,"axis"],"(x, center, ", x$params[,"R"], ",", x$params[,"r0"] ,");", "\n",
+         x$name, " = torus;", "\n",
+         "}"
+  )
+}
+
+length.HyperFunTorus <- function(x, ...){
+  nrow(x$param)  
+}
+
+## hfEllipticCone
+
+hfElliCone <- function(cx = 0, cy = 0, cz = 0, a = 0.5, b = 0.5, axis = "X", name = NULL)
+{
+  if (!(is.numeric(cx) &&
+        is.numeric(cy) &&
+        is.numeric(cz) &&
+        is.numeric(a)  &&
+        is.numeric(b)))
+    stop("All arguments must be numeric")
+  axis = toupper(axis)
+  if (!all(axis %in% c("X", "Y", "Z")))
+  {
+    stop("Axis name must be 'X' or 'Y' or 'Z'")
+  }
+  params <- cbind(cx, cy, cz, a, b, axis)
+  colnames(params) <- c("x", "y", "z", "a", "b", "axis")
+  name <- hfName(name, nrow(params))
+  ellCone <- list(params = params, name = name)
+  class(ellCone) <- c("HyperFunEllipticCone", "HyperFunObject", "HyperFun")
+  ellCone
+}
+
+as.character.HyperFunEllipticCone <- function(x, ...)
+{
+  center <- paste0("[", x$params[,"x"], ",", x$params[,"y"], "," , x$params[,"z"], "]")
+  paste0(x$name, "(x[3], a[1])", "\n",
+         "{", "\n",
+         "array center[3];", "\n",
+         "center = ", center, ";", "\n",
+         "ellcone = hfEllCone", x$params[,"axis"],"(x, center, ", x$params[,"a"], ",", x$params[,"b"] ,");", "\n",
+         x$name, " = ellcone;", "\n",
+         "}"
+  )
+}
+
+length.HyperFunEllipticCone <- function(x, ...){
+  nrow(x$param)  
+}
+
+## hfCone
+hfCone <- function(cx = 0, cy = 0, cz = 0, r = 0.5, axis = "X", name = NULL)
+{
+  if (!(is.numeric(cx) &&
+        is.numeric(cy) &&
+        is.numeric(cz) &&
+        is.numeric(r)))
+    stop("All arguments must be numeric")
+  axis = toupper(axis)
+  if (!all(axis %in% c("X", "Y", "Z")))
+  {
+    stop("Axis name must be 'X' or 'Y' or 'Z'")
+  }
+  params <- cbind(cx, cy, cz, r, axis)
+  colnames(params) <- c("x", "y", "z", "r", "axis")
+  name <- hfName(name, nrow(params))
+  cone <- list(params = params, name = name)
+  class(cone) <- c("HyperFunCone", "HyperFunObject", "HyperFun")
+  cone
+}
+
+as.character.HyperFunCone <- function(x, ...)
+{
+  center <- paste0("[", x$params[,"x"], ",", x$params[,"y"], "," , x$params[,"z"], "]")
+  paste0(x$name, "(x[3], a[1])", "\n",
+         "{", "\n",
+         "array center[3];", "\n",
+         "center = ", center, ";", "\n",
+         "cone = hfCone", x$params[,"axis"],"(x, center, ", x$params[,"r"], ");", "\n",
+         x$name, " = cone;", "\n",
+         "}"
+  )
+}
+
+length.HyperFunCone <- function(x, ...){
+  nrow(x$param)  
+}
+
+
+## hfelicylinder
+
+hfElliCylinder <- function(cx = 0, cy = 0, cz = 0, a = 2, b = 4, axis = "X", name = NULL)
+{
+  if (!(is.numeric(cx) &&
+        is.numeric(cy) &&
+        is.numeric(cz) &&
+        is.numeric(a)  &&
+        is.numeric(b)))
+    stop("All arguments must be numeric")
+  axis = toupper(axis)
+  if (!all(axis %in% c("X", "Y", "Z")))
+  {
+    stop("Axis name must be 'X' or 'Y' or 'Z'")
+  }
+  params <- cbind(cx, cy, cz, a, b, axis)
+  colnames(params) <- c("x", "y", "z", "a", "b", "axis")
+  name <- hfName(name, nrow(params))
+  ellCylinder <- list(params = params, name = name)
+  class(ellCylinder) <- c("HyperFunEllipticCylinder", "HyperFunObject", "HyperFun")
+  ellCylinder
+}
+
+as.character.HyperFunEllipticCylinder <- function(x, ...)
+{
+  center <- paste0("[", x$params[,"x"], ",", x$params[,"y"], "," , x$params[,"z"], "]")
+  paste0(x$name, "(x[3], a[1])", "\n",
+         "{", "\n",
+         "array center[3];", "\n",
+         "center = ", center, ";", "\n",
+         "ellcylinder = hfEllCyl", x$params[,"axis"],"(x, center, ", x$params[,"a"], ",", x$params[,"b"] ,");", "\n",
+         x$name, " = ellcylinder;", "\n",
+         "}"
+  )
+}
+
+length.HyperFunEllipticCylinder <- function(x, ...){
+  nrow(x$param)  
+}
+
+## hfcylinder
+hfCylinder <- function(cx = 0, cy = 0, cz = 0, r = 1, axis = "X", name = NULL)
+{
+  if (!(is.numeric(cx) &&
+        is.numeric(cy) &&
+        is.numeric(cz) &&
+        is.numeric(r)))
+    stop("All arguments must be numeric")
+  axis = toupper(axis)
+  if (!all(axis %in% c("X", "Y", "Z")))
+  {
+    stop("Axis name must be 'X' or 'Y' or 'Z'")
+  }
+  params <- cbind(cx, cy, cz, r, axis)
+  colnames(params) <- c("x", "y", "z", "r", "axis")
+  name <- hfName(name, nrow(params))
+  cylinder <- list(params = params, name = name)
+  class(cylinder) <- c("HyperFunCylinder", "HyperFunObject", "HyperFun")
+  cylinder
+}
+
+as.character.HyperFunCylinder <- function(x, ...)
+{
+  center <- paste0("[", x$params[,"x"], ",", x$params[,"y"], "," , x$params[,"z"], "]")
+  paste0(x$name, "(x[3], a[1])", "\n",
+         "{", "\n",
+         "array center[3];", "\n",
+         "center = ", center, ";", "\n",
+         "cylinder = hfCylinder", x$params[,"axis"],"(x, center, ", x$params[,"r"], ");", "\n",
+         x$name, " = cylinder;", "\n",
+         "}"
+  )
+}
+
+length.HyperFunCylinder <- function(x, ...){
+  nrow(x$param)  
+}
+
+## hfellipsoid
+hfEllipsoid <- function(cx = 0, cy = 0, cz = 0, a = 1, b = 1, c = 1, name = NULL)
+{
+  if (!(is.numeric(cx) &&
+        is.numeric(cy) &&
+        is.numeric(cz) &&
+        is.numeric(a)  &&
+        is.numeric(b)  &&
+        is.numeric(c)))
+    stop("All arguments must be numeric")
+  params <- cbind(cx, cy, cz, a, b, c)
+  colnames(params) <- c("x", "y", "z", "a", "b", "c")
+  name <- hfName(name, nrow(params))
+  ellipsoid <- list(params = params, name = name)
+  class(ellipsoid) <- c("HyperFunEllipsoid", "HyperFunObject", "HyperFun")
+  ellipsoid
+}
+
+as.character.HyperFunEllipsoid <- function(x, ...)
+{
+  center <- paste0("[", x$params[,"x"], ",", x$params[,"y"], "," , x$params[,"z"], "]")
+  halfAxes <- paste(x$params[, "a"], x$params[, "b"],  x$params[, "c"], sep = ",")
+  paste0(x$name, "(x[3], a[1])",
+         "{",
+         "array center[3];",
+         "center = ", center, ";",
+         "ellipsoid = hfEllipsoid(x, center, ", halfAxes, ");",
+         x$name, " = ellipsoid;",
+         "}"
+  )
+}
+
+length.HyperFunEllipsoid <- function(x, ...){
+  nrow(x$param)  
+}
+
+
+## hfSphere
 hfSphere <- function(cx = 0, cy = 0, cz = 0, r = 1, name = NULL) {
     if (!(is.numeric(cx) &&
           is.numeric(cy) &&
@@ -78,3 +352,4 @@ as.character.HyperFunBlock <- function(x, ...) {
 length.HyperFunBlock <- function(x, ...) {
     nrow(x$params)
 }
+
